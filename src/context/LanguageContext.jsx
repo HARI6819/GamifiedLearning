1d5b14858d0316aae2c6ae2d79cf800e2c57fa30
HARI6819 <haridhivya22@gmail.com>
2026-02-19 15:25:46 +0530

    translation update

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext } from 'react';
-import { translations } from '../data/translations';
+import translations from '../data/translations';
 
 const LanguageContext = createContext();
 
